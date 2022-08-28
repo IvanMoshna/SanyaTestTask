254e322ca4a0870f0cae9e272d3234b8d94976d5
moshn <moshna.ivan@gmail.com>
2022-08-28 09:08:35 +0700

    add example flags

--- TestTask/src/main/java/Main.java
+++ TestTask/src/main/java/Main.java
@@ -16,14 +16,34 @@ public class Main {
         String[] subStr;
         subStr = path.split(" ");
 
- /*       for (int i=0; i < subStr.length; i++) {
-            System.out.println(i + " " + subStr[i]);
-        }*/
 
-        //s4itaem chto
-        //0 - eto sort.exe
-        //1 - flag
-        //ot 2 do n - puti
+        //TODO: напиздел я сам себе, короче когда парсим, нужно смотреть первый символ, если это "-", значит это флаг, если не минус, значит путь,
+        //потому что 2 флага может идти подряд
+        //может, конечно, есть какие то более логичные и лучше работающие способы, но на ум сразу только такая идея пришла.
+        //плюс это тестовое на курсы блять, чо они хотят, файл считал и заебись.
+
+        //TODO: тут еще можно смотреть, что скажем у нас всего одна буква, если больше, то писать, шо Миша все хуйня, давай по новой
+        for (int i=0; i < subStr.length; i++) {
+            if(subStr[i].substring(0, 1).equals("-")){
+                switch (subStr[i].substring(1, 2)){
+                    case "i":
+                        System.out.println("Пацаны тут интов нам привезли");
+                        break;
+                    case "s":
+                        System.out.println("Разгружай вагон со стрингами");
+                        break;
+                    case "a":
+                        System.out.println("Давай вира");
+                        break;
+                    case "d":
+                        System.out.println("Майна, майна!");
+                        break;
+                }
+            }
+        }
+
+
+
         FileService fileService = new FileService();
         File f = fileService.getFileFromPath(subStr[0]);
         if(f==null){
@@ -32,6 +52,8 @@ public class Main {
 
         //System.out.println(f.getName());
         //fileService.createFile();
+        fileService.copyFileContent("C:\\Users\\moshn\\Downloads\\Telegram Desktop\\TestTask\\TestTask\\src\\main\\resources\\in1.txt",
+                "C:\\Users\\moshn\\Downloads\\Telegram Desktop\\TestTask\\TestTask\\src\\main\\resources\\out.txt");
 
     }
 
